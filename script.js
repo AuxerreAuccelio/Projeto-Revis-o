9f26b03add6386fcfad377ddec00eb41cac1d3c5
AuxerreAuccelio <auxerreauccelio9@gmail.com>
2026-05-19 22:15:35 -0300

    refatoracao imagem abilidades na tela

--- script.js
+++ script.js
@@ -1,7 +1,86 @@
-// const url = `https://picsum.photos/v2/list`; //url retirada do site
-// //const url = `https://picsum.photos/200`;
+// const url = `https://pokeapi.co/api/v2/pokemon/`; //url retirada do site
+// //const url = `https://pokeapi.co/api/v2/pokemon/4`;
 
-// //const main_html = document.querySelector('main');
+async function get_willing(e){
+    e.preventDefault();
+    const pokemon = document.querySelector("#top input");   //aqui devolve um objeto apontando para o HTML
+    // console.log(pokemon);
+    const id_pokemon = (pokemon.value.toLowerCase().trim());   // aqui devolve um number
+    // console.log(id_pokemon);
+
+    if(id_pokemon)
+        await data(id_pokemon);
+} 
+
+document.querySelector('form').addEventListener('submit', get_willing );
+
+
+async function data(id_pokemon){
+    const url = `https://pokeapi.co/api/v2/pokemon/${id_pokemon}`;   //atribui-se o id ao final da url
+    // console.log(url);
+    await metadados(url);
+}
+
+async function metadados(url){
+    const url2 = await fetch(url);   // a url string é transformada em objeto e fala sobre
+                                     // a trasmissão do status 200 ok ou 404 not found
+    // console.log(url2);
+    const url_json = await url2.json();   // a url objeto mostra os metadados de fato da API
+    console.log(url_json); 
+    refresh_screen(url_json);
+}
+
+
+function refresh_screen(pokemon_tratado){
+    const img = document.querySelector('section img');
+    img.src = pokemon_tratado.sprites.other['official-artwork'].front_shiny;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const url = `https://picsum.photos/v2/list`; 
 
 // async function data(){
 //     const dados = await fetch(url);
